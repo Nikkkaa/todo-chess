a7a7712fe88cf1069d394a86d1f00bdb41054f0f
Nikita Kalmykov <nik.kalmykov.1986@mail.ru>
2023-04-29 20:51:53 +0300

    made Board

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import BoardComponent from "./components/BoardComponent";
+import { Board } from "./models/Board";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [board, setBoard] = useState(new Board());
+
+  useEffect(() => {
+    restart();
+  }, []);
+
+  function restart() {
+    const newBoard = new Board();
+    newBoard.initCells();
+    setBoard(newBoard);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <BoardComponent board={board} setBoard={setBoard}></BoardComponent>
     </div>
   );
-}
+};
 
 export default App;
